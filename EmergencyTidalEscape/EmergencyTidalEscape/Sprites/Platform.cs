e243e6e42700a42a1df927a553906a39826c0498
GeorgeM <G.MORTER-2021@hull.ac.uk>
2023-11-25 11:12:11 +0000

    starting work on platform collision

---
+++ EmergencyTidalEscape/EmergencyTidalEscape/Sprites/Platform.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmergencyTidalEscape.Sprites
+{
+    internal class Platform : Sprite
+    {
+        private Game1 _root;
+
+        public Platform(Game1 root, Vector2 Position, float width) : base(Position) 
+        { 
+            _root = root;
+            this.Position = Position;
+            this.SpriteWidth = width;
+        }
+
+
+    }
+}
